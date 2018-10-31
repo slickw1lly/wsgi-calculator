aff779579d23fa587e6ff6540266e4bdf5e2d7f6
Kim Truong <35829229+slickw1lly@users.noreply.github.com>
2018-10-30 23:11:58 -0700

    Finished wsgi-calculator

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import traceback
 """
 For your homework this week, you'll be creating a wsgi application of
 your own.
@@ -37,9 +38,28 @@ To submit your homework:
     that explains how to perform calculations.
   * Commit and push your changes to your fork.
   * Submit a link to your Session03 fork repository!
-
-
 """
+
+
+def home(*args):
+    """Return body of home page."""
+
+    m = 'http://localhost:8080/multiply/3/5'
+    a = 'http://localhost:8080/add/23/42'
+    s = 'http://localhost:8080/subtract/23/42'
+    d = 'http://localhost:8080/divide/22/11'
+
+    body = """
+        <html>Here's how to use this page...</html>
+        <br>
+        <br>
+        <h>Example:</h><br>
+        <a href="{0}">{0}</a><br>
+        <a href="{1}">{1}</a><br>
+        <a href="{2}">{2}</a><br>
+        <a href="{3}">{3}</a><br>
+    """.format(m, a, s, d)
+    return body
 
 
 def add(*args):
@@ -47,11 +67,28 @@ def add(*args):
 
     # TODO: Fill sum with the correct value, based on the
     # args provided.
-    sum = "0"
-
-    return sum
+    return str(args[0] + args[1])
 
 # TODO: Add functions for handling more arithmetic operations.
+
+
+def subtract(*args):
+    """ Returns a STRING with the subtraction of the arguments """
+    return str(args[0] - args[1])
+
+
+def multiply(*args):
+    """ Returns a STRING with the multiplication of the arguments """
+    return str(args[0] * args[1])
+
+
+def divide(*args):
+    """ Returns a STRING with the division of the arguments """
+    try:
+        return str(args[0] / args[1])
+    except ZeroDivisionError:
+        return 'BAM, GG!'
+
 
 def resolve_path(path):
     """
@@ -63,10 +100,22 @@ def resolve_path(path):
     # examples provide the correct *syntax*, but you should
     # determine the actual values of func and args using the
     # path.
-    func = add
-    args = ['25', '32']
-
+    funcs = {
+        '': home,
+        'add': add,
+        'subtract': subtract,
+        'multiply': multiply,
+        'divide': divide
+    }
+    path = path.strip('/').split('/')
+    func_name = path[0]
+    args = [float(arg) for arg in path[1:]]
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
     return func, args
+
 
 def application(environ, start_response):
     # TODO: Your application code from the book database
@@ -76,9 +125,30 @@ def application(environ, start_response):
     #
     # TODO (bonus): Add error handling for a user attempting
     # to divide by zero.
-    pass
+
+    headers = [("Content-type", "text/html")]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
 
 if __name__ == '__main__':
     # TODO: Insert the same boilerplate wsgiref simple
     # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
